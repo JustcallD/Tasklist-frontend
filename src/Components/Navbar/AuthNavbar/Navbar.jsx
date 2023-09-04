08f53a41a8318da2f40cf138b05a79cab0c7697e
Mahajan Dhiraj <mahajandhiru001@gmail.com>
2023-09-04 20:07:15 +0530

    some changes in navbar

--- src/Components/Navbar/AuthNavbar/Navbar.jsx
+++ src/Components/Navbar/AuthNavbar/Navbar.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const containerClassName = `${styles.Container} ${
-    menuOpen ? styles.grayBackground : ""
-  }`;
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -15,6 +29,11 @@ const Navbar = () => {
   const closeMenu = () => {
     setMenuOpen(false);
   };
+
+  const containerClassName = `${styles.Container} ${
+    (menuOpen || scrolling) && styles.grayBackground
+  }`;
+
   return (
     <nav className={containerClassName}>
       <div className={styles.logo}>
