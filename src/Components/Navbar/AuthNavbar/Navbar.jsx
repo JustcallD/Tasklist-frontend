e60855cff4673a41362e78d8f949f073f14fa301
Mahajan Dhiraj <mahajandhiru001@gmail.com>
2023-09-04 19:18:48 +0530

    automatic close menu added

--- src/Components/Navbar/AuthNavbar/Navbar.jsx
+++ src/Components/Navbar/AuthNavbar/Navbar.jsx
@@ -12,10 +12,15 @@ const Navbar = () => {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
   return (
     <nav className={containerClassName}>
       <div className={styles.logo}>
-        <Link to={"/"}>Logo</Link>
+        <Link to={"/"} onClick={closeMenu}>
+          Logo
+        </Link>
         <div className={styles.navMenu} onClick={toggleMenu}>
           <FaBars className={styles.menuIcon} />
         </div>
@@ -23,16 +28,24 @@ const Navbar = () => {
 
       <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
         <li className={styles.navLink}>
-          <NavLink to={"/login"}>Login</NavLink>
+          <NavLink to={"/login"} onClick={closeMenu}>
+            Login
+          </NavLink>
         </li>
         <li className={styles.navLink}>
-          <NavLink to={"/signup"}>SignUp</NavLink>
+          <NavLink to={"/signup"} onClick={closeMenu}>
+            SignUp
+          </NavLink>
         </li>
         <li className={styles.navLink}>
-          <NavLink to={"/contact"}>contact</NavLink>
+          <NavLink to={"/contact"} onClick={closeMenu}>
+            contact
+          </NavLink>
         </li>
         <li className={styles.navLink}>
-          <NavLink to={"/tasks"}>tasks</NavLink>
+          <NavLink to={"/tasks"} onClick={closeMenu}>
+            tasks
+          </NavLink>
         </li>
       </ul>
     </nav>
