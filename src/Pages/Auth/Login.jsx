a8d5064a3d50659d7acf8333114df6a45b9501a8
Mahajan Dhiraj <mahajandhiru001@gmail.com>
2023-09-04 14:58:57 +0530

    basic login and signup system is added

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,15 +1,124 @@
-import React, { useState } from "react";
-import styles from "./Login.module.css";
-import { Link } from "react-router-dom";
+// import React, { useState } from "react";
+// import styles from "./Login.module.css";
+// import { Link, useNavigate } from "react-router-dom";
 // import axios from "axios";
 
+// const Login = () => {
+//   const navigate = useNavigate();
+
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleSubmit = async () => {
+//     try {
+//       const headers = {
+//         "Content-Type": "application/json",
+//       };
+//       const response = await axios.post(
+//         "http://localhost:4000/v1/auth/login",
+//         {
+//           email,
+//           password,
+//         },
+//         { headers }
+//       );
+//       const data = response.data; // Get the response data
+//       console.log(data);
+//       navigate("/");
+//     } catch (error) {
+//       // Handle errors here
+//       console.error("Error:", error);
+//     }
+//   };
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.subContainer}>
+//         <div className={styles.detailsSection}>
+//           <div className={styles.company}>Company Name</div>
+//           <div className={styles.moto}>
+//             Join us and be a part of our community.
+//           </div>
+//           <Link to="/signup" className={styles.detailsSectionbtn}>
+//             Join Us
+//           </Link>
+//         </div>
+//         <div className={styles.loginForm}>
+//           <h1 className={styles.loginFormTitle}>Login</h1>
+//           <form method="POST" onSubmit={handleSubmit}>
+//             <div className={styles.loginFormInput}>
+//               <label className={styles.loginFormLable}>Email</label>
+//               <input
+//                 className={styles.loginFormInput}
+//                 type="text"
+//                 placeholder="Email"
+//                 name="email"
+//                 value={email}
+//                 onChange={(e) => {
+//                   setEmail(e.target.value);
+//                 }}
+//               />
+//             </div>
+//             <div className={styles.loginFormInput}>
+//               <label className={styles.loginFormLable}>Password</label>
+//               <input
+//                 className={styles.loginFormInput}
+//                 type="text"
+//                 placeholder="Password"
+//                 name="password"
+//                 value={password}
+//                 onChange={(e) => {
+//                   setPassword(e.target.value);
+//                 }}
+//               />
+//             </div>
+//             <div className={styles.loginFormbtn}>
+//               <button type="submit">submit</button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the default form submission
+
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      // Pass the email and password data in the request body
+      const response = await axios.post(
+        "http://localhost:4000/v1/auth/login",
+        {
+          email,
+          password,
+        },
+        { headers }
+      );
+      const data = response.data;
+      console.log(data);
+      navigate("/");
+    } catch (error) {
+      // Handle errors here
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.subContainer}>
@@ -24,7 +133,7 @@ const Login = () => {
         </div>
         <div className={styles.loginForm}>
           <h1 className={styles.loginFormTitle}>Login</h1>
-          <form method="POST" onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className={styles.loginFormInput}>
               <label className={styles.loginFormLable}>Email</label>
               <input
@@ -42,7 +151,7 @@ const Login = () => {
               <label className={styles.loginFormLable}>Password</label>
               <input
                 className={styles.loginFormInput}
-                type="text"
+                type="password"
                 placeholder="Password"
                 name="password"
                 value={password}
@@ -52,7 +161,7 @@ const Login = () => {
               />
             </div>
             <div className={styles.loginFormbtn}>
-              <button type="submit">submit</button>
+              <button type="submit">Submit</button>
             </div>
           </form>
         </div>
--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -1,9 +1,41 @@
-import React from "react";
-import styles from "./Signup.module.css"; 
-import { Link } from "react-router-dom"; 
+import React, { useState } from "react";
+import styles from "./Signup.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = () => {
-  const handleSubmit = () => {};
+  const navigate = useNavigate();
+  const [username, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the default form submission
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      if (password !== confirmPassword) {
+        console.error("Passwords do not match");
+        return;
+      }
+      const response = await axios.post(
+        "http://localhost:4000/v1/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        { headers }
+      );
+      const data = response.data;
+      console.log(data);
+      navigate("/login");
+    } catch (error) {
+      // Handle errors here
+      console.error("Error:", error);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.subContainer}>
@@ -14,19 +46,21 @@ const Signup = () => {
               If you are already part of our family, please login below.
             </p>
           </div>
-          <Link to="/login" className={styles.loginBtn}>
+          <Link to="/login" className={styles.detailsSectionbtn}>
             Login
           </Link>
         </div>
         <div className={styles.signupForm}>
           <h1 className={styles.formTitle}>Sign Up</h1>
-          <form method="POST" onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className={styles.formInput}>
               <label className={styles.formLabel}>Full Name</label>
               <input
                 className={styles.formField}
                 type="text"
                 placeholder="Full Name"
+                value={username}
+                onChange={(e) => setFullName(e.target.value)}
               />
             </div>
             <div className={styles.formInput}>
@@ -35,6 +69,8 @@ const Signup = () => {
                 className={styles.formField}
                 type="email"
                 placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className={styles.formInput}>
@@ -43,6 +79,8 @@ const Signup = () => {
                 className={styles.formField}
                 type="password"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className={styles.formInput}>
@@ -51,6 +89,8 @@ const Signup = () => {
                 className={styles.formField}
                 type="password"
                 placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
             <div className={styles.formBtn}>
